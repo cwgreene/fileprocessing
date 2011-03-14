f3d685347d066998d9fccd1ac08db6a8df53a231
Chris Greene <archgoon@gmail.com>
2011-03-14 10:06:47 -0700

    more.

--- line_lengths.py
+++ line_lengths.py
@@ -2,4 +2,9 @@
 #list their lengths in pixels
 import lxml
 
-
+def get_lines(filename="test.xml"):
+	tree = lxml.ElementTree.parse(filename)
+	result = []
+	for v in tree.findall("//line"):
+		print v.value
+get_lines()
