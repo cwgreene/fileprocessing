49a387e006fe228a1dc8874e28240f9aa21d85fc
Chris <archgoon@gmail.com>
2011-03-14 10:18:18 -0700

    Base line functionality. Making xml well formed is annoying.

--- line_lengths.py
+++ line_lengths.py
@@ -1,10 +1,11 @@
 #process svg documents and extract all lines,
 #list their lengths in pixels
-import lxml
+from lxml import etree
 
 def get_lines(filename="test.xml"):
-	tree = lxml.ElementTree.parse(filename)
+	tree = etree.parse(filename)
 	result = []
 	for v in tree.findall("//line"):
-		print v.value
+		print v.tag
+		print v.get("value")
 get_lines()
